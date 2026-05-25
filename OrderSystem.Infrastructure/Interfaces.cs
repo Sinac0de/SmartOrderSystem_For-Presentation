a206f3a252ef1a61909f16a4088b38c905fac31b
Sina Moradian <sinacsharp@gmail.com>
2026-05-25 07:21:01 +0330

    Add IDataService and IAuthService interfaces to improve service abstraction

---
+++ OrderSystem.Infrastructure/Interfaces.cs
@@ -0,0 +1,12 @@
+﻿using OrderSystem.Core;
+
+namespace OrderSystem.Infrastructure {
+    // [Refactored: Dependency Inversion Principle]
+    public interface IDataService {
+        void SaveOrder(Order order);
+    }
+
+    public interface IAuthService {
+        string HashPassword(string plainText);
+    }
+}
