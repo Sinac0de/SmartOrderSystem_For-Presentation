f39a70bcfb8b9471a0e537074bd9de35b95b7f0d
Sina Moradian <sinacsharp@gmail.com>
2026-05-28 05:29:21 +0330

    Add unit tests for CleanOrderProcessor and MockDataService; update project references

---
+++ OrderSystem.Tests/MockDataService.cs
@@ -0,0 +1,11 @@
+﻿using OrderSystem.Core;
+using OrderSystem.Infrastructure;
+
+namespace OrderSystem.Tests {
+    //  This mock will simulate the behavior of the IDataService without performing any actual data operations.
+    public class MockDataService : IDataService {
+        public void SaveOrder(Order order) {
+            // Nothing ;)
+        }
+    }
+}
